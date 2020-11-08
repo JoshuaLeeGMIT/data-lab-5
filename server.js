4b90a2206874a4c8e76aca6af19915b50889b45f
Joshua Lee <lee.son.wai@gmail.com>
2020-11-08 01:40:14 +0000

    server: Add POST example to form

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Welcome to Data Representation & Querying');
@@ -37,6 +41,10 @@ app.get('/test', (req, res) => {
 
 app.get('/name', (req, res) => {
   res.send('Hello ' + req.query.fName + ' ' + req.query.lName + '!');
+})
+
+app.post('/name', (req, res) => {
+  res.send('Hello, ' + req.body.fName + ' ' + req.body.lName + '!');
 })
 
 app.listen(port, () => {
