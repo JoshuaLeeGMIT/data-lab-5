3dbf03146e6fa6a2aa162e55911a83fb547c7a37
Joshua Lee <lee.son.wai@gmail.com>
2020-11-08 01:07:18 +0000

    server: Add argument handling example

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to Data Representation & Querying');
 })
 
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello, ' + req.params.name + '!');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
